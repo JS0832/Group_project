86758907b7da43964101f5b47849700a041d4fdb
JopStro <joseph.strong@live.co.uk>
2022-03-01 12:48:54 +0000

    Re-commit database implementation

--- mysite/websiteApp/models.py
+++ mysite/websiteApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uid = models.IntegerField()
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    total_points = models.IntegerField()
+    game_master = models.BooleanField()
+    
+class Riddles(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    time = models.DateTimeField('date published')
+    points = models.IntegerField()
+    likes = models.IntegerField()
+    
+class Score(models.Model):
+    username = models.CharField(max_length=30)
+    time = models.DateTimeField('date published')
+    points = models.IntegerField()
