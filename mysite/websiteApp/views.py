6a2946ae5e18f0ede52ae693bac8944afd26813d
Paul Workman <pw466@exeter.ac.uk>
2022-03-02 18:15:26 +0000

    Moved files to functioning spots. Fixed navigation errors. General website improvement.

--- mysite/websiteApp/views.py
+++ mysite/websiteApp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import libraries.websiteApp.codebase as game
+import websiteApp.lib.websiteApp.codebase as gameBase
 
 def index(request):
     # Render simply returns the html, add contect for personalised changes
@@ -11,3 +11,21 @@ def login(request):
 
 def register(request):
     return render(request, 'websiteApp/register.html')
+
+def game(request):
+    return render(request, 'websiteApp/game.html')
+
+def location(request):
+    return render(request, 'websiteApp/location.html')
+
+def campus_exploration(request):
+    return render(request, 'websiteApp/campus_exploration.html')
+
+def main(request):
+    return render(request, 'websiteApp/main.html')
+
+def settings(request):
+    return HttpResponse(request, "Settings TODO")
+
+def introduce(request):
+    return HttpResponse(request, "Introduction TODO")
