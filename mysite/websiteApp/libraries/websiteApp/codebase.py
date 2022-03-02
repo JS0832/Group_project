2d3089dc6d70a14fa9401eeeb75754d4666129c9
Paul Workman <pw466@exeter.ac.uk>
2022-03-02 16:21:08 +0000

    Docstring improved

--- mysite/websiteApp/libraries/websiteApp/codebase.py
+++ mysite/websiteApp/libraries/websiteApp/codebase.py
@@ -5,17 +5,18 @@
 # code and also improve readability of views.
 # Add functions as necessary!
 
-def stringComparison(stringA: str, stringB: str) -> bool:
-    """Compares two strings, removing some character, returns True if the same
+def stringComparison(stringA: str, stringB: str, charRemove: list = [" ","-",",","."]) -> bool:
+    """Compares two strings, removing some characters.
+    Returns True if the same
 
-    Keyword Arguments:
     stringA : str
     stringB : str
+    charRemove : list (default = [" ","-",",","."])
+        A list of characters to remove from both strings
     """
-    characterToRemove = [" ", "-", ",", "."]
         #Any character that may cause discrepancies between correct inputs.
 
-    for character in characterToRemove:
+    for character in charRemove:
         # Remove the unnecessary characters by replacing them with nothing
         stringA = stringA.replace(character, '')
         stringB = stringB.replace(character, '')
