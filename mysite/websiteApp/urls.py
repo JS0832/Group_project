e91c9382d253ab14044967b84467494977d80cc3
Mezann <65676830+Mezann@users.noreply.github.com>
2022-03-21 12:56:24 +0000

    Working on logout functionality

--- mysite/websiteApp/urls.py
+++ mysite/websiteApp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('game/', views.game, name='game'),
