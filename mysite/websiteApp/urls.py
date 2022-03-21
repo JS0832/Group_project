b1d22559b5dc3d0669f101ef8f431f193ec97a9c
Mezann <65676830+Mezann@users.noreply.github.com>
2022-03-21 13:26:44 +0000

    Fixing

--- mysite/websiteApp/urls.py
+++ mysite/websiteApp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('game/', views.game, name='game'),
