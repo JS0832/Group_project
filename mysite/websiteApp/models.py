3655b64ab3c24c51162815f795c5634450aa870a
KrisTeng0 <100372020+KrisTeng0@users.noreply.github.com>
2022-03-23 19:26:07 +0000

    minor tweaks with styles

--- mysite/websiteApp/models.py
+++ mysite/websiteApp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Access through User.profile.(field)
 # Created with help from:
 # simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
@@ -12,17 +13,18 @@ class Profile(models.Model):
     game_master = models.BooleanField(default=False)
 
 
-#Taken from:
-#simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+# Taken from:
+# simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
+
 
 class Riddle(models.Model):
     question = models.TextField(max_length=250)
@@ -30,7 +32,8 @@ class Riddle(models.Model):
     time = models.DateTimeField('date published')
     points = models.IntegerField()
     likes = models.IntegerField()
-    
+
+
 class Score(models.Model):
     username = models.CharField(max_length=30)
     time = models.DateTimeField('date published')
